33fdb6883672798b2fd4c31a49a8ace08e954be3
lakinsm <steven.m.lakin@gmail.com>
2021-05-06 10:27:00 -0500

    Nanopore metadata

--- bin/plot_nanopore_metadata.py
+++ bin/plot_nanopore_metadata.py
@@ -317,7 +317,7 @@ def format_alignment_data(sam, ref):
 				covlist += [cov_val for _ in range(idx_min - cur_len)]
 			cur_len = idx_min
 		for i in range(len(targets)):
-			print(targets[i], cigars[i], starts[i], revs[i])
+			print(cigars[i], parse_cigar(cigars[i], starts[i], revs[i]))
 			cov[targets[i]].add(parse_cigar(cigars[i], starts[i], revs[i]))
 	last_cov = [(v[-1], k) for k, v in ret.items()]
 	print(last_cov)
